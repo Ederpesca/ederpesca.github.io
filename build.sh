1217051851d624004a41e6dfd71b9be80da15760
Iñaki García Blasco <inaki@txorua.com>
2017-12-13 19:58:14 +0100

    Creado workflow de Travis CI

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# only proceed script when started not by pull request (PR)
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# build site with jekyll, by default to `_site' folder
+bundle exec jekyll build
+
+# copy generated HTML site to `master' branch
+export RSYNC_PASSWORD=${password}
+rsync -avz --delete _site/ ${username}@ederpesca.com:www/
